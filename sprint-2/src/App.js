d00867d8fb3ffd201230bc7d4817ef9e43ea8dc4
Jessica Kwok <jessicatckwok@gmail.com>
2020-06-22 17:59:40 -0700

    installed axios and react-router-dom - added routes and links to go to/from video player page and upload page

--- sprint-2/src/App.js
+++ sprint-2/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import Header from './components/header/Header';
-import Hero from './components/hero/Hero';
-import Main from './components/main/Main';
 import data from './data.json';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import VideoPlayer from './pages/video-player/VideoPlayer';
+import Upload from './pages/upload/Upload';
 
 class App extends React.Component {
   state = {data};
 
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Hero src={process.env.PUBLIC_URL + this.state.data[1][0].video} poster={process.env.PUBLIC_URL + this.state.data[1][0].image} duration={this.state.data[1][0].duration} />
-        <Main mainVideo={this.state.data[1][0]} sideVideos={this.state.data[0]} />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={VideoPlayer} />
+          <Route path="/upload" component={Upload} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
