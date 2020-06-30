3f3fcc7d36a6ad7712d20fb6cd822b8ae475c528
Jessica Kwok <jessicatckwok@gmail.com>
2020-06-29 19:54:00 -0700

    restructured data.json file, adjusted get /video and /video:id endpoints to reflect data structure change, completed post /video endpoint

--- sprint-3/client/src/pages/Upload/Upload.jsx
+++ sprint-3/client/src/pages/Upload/Upload.jsx
@@ -1,54 +1,73 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Header from '../../components/Header/Header';
 import './upload.scss';
 import Axios from 'axios';
 
 const url = 'http://localhost:8080/videos';
 
-const Upload = () => {
+class Upload extends React.Component {
+    state = {
+        posted: false
+    }
 
-    const postNewVideo = (e) => {
-        e.preventDefault();
-        console.log('click');
+    postNewVideo = (e) => {
+        e.preventDefault(); 
+        this.setState({ posted: true });
+        const id = uuidv4();
         Axios
-        .post(`${url}`,
+        .post(url,
         {
-            id: '',
+            id: id,
             title: e.target.title.value,
             channel: "Mohan Muruge",
             image: process.env.PUBLIC_URL + '/assets/images/upload-video-preview.jpg',
+            description: e.target.description.value,
+            views: 0,
+            likes: 0,
+            duration: "2:37",
+            video: 'https://project-2-api.herokuapp.com/stream',
+            timestamp: Date.now(),
+            comments: []
         })
         .then(res => {
-            console.log(res);
-            e.target.reset();
+            console.log(res)
         })
+        .catch(err => {
+            console.error(err);
+        })
+        e.target.reset();
     }
 
-    return (
-        <div>
-            <Header />
-            <main className="upload">
-                <h2 className="upload__heading">Upload Video</h2>
-                <div className="upload__detail-container">
-                    <div className="upload__thumbnail-container">
-                        <p className="upload__label">VIDEO THUMBNAIL</p>
-                        <img className="upload__preview" src={process.env.PUBLIC_URL + '/assets/images/upload-video-preview.jpg'} alt="upload thumbnail preview" />
-                    </div>
-                    <form className="upload__form" onSubmit={(e) => {postNewVideo(e)}} >
-                        <label htmlFor="title" className="upload__label">TITLE YOUR VIDEO</label>
-                        <input type="text" name="title" className="upload__title-input" placeholder="Add a title to your video"></input>
-                        <label htmlFor="description" className="upload__label">ADD A VIDEO DESCRIPTION</label>
-                        <textarea name="description" className="upload__description-input" placeholder="Add a description of your video"></textarea>
-                        <div className="upload__buttons-container">
-                            <button type="submit" className="upload__button">PUBLISH</button>
-                            <button className="upload__cancel">CANCEL</button>
+    render() {
+        return (
+            <div>
+                <Header />
+                <main className="upload">
+                    <h2 className="upload__heading">Upload Video</h2>
+                    <div className="upload__detail-container">
+                        <div className="upload__thumbnail-container">
+                            <p className="upload__label">VIDEO THUMBNAIL</p>
+                            <img className="upload__preview" src={process.env.PUBLIC_URL + '/assets/images/upload-video-preview.jpg'} alt="upload thumbnail preview" />
                         </div>
-                    </form>
-                </div>
-                
-            </main>
-        </div>
-    )
+                        <form className="upload__form" onSubmit={(e) => {this.postNewVideo(e)}} >
+                            <label htmlFor="title" className="upload__label">TITLE YOUR VIDEO</label>
+                            <input required type="text" name="title" className="upload__title-input" placeholder="Add a title to your video"></input>
+                            <label htmlFor="description" className="upload__label">ADD A VIDEO DESCRIPTION</label>
+                            <textarea required name="description" className="upload__description-input" placeholder="Add a description of your video"></textarea>
+                            <div className="upload__buttons-container">
+                                <button type="submit" className="upload__button">PUBLISH</button>
+                                <button type="button" className="upload__cancel">CANCEL</button>
+                            </div>
+                        </form>
+                        {
+                            this.state.posted ? <div className="upload__success">Posted successfully.</div> : null
+                        }
+                    </div>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default Upload;
--- sprint-3/server/server.js
+++ sprint-3/server/server.js
@@ -2,23 +2,38 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const videoList = require('./videoList.json');
-const videoDetail = require('./videoDetail.json')
+const data = require('./data.json');
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-// GET /videos - array of side videos
+// GET /videos 
 app.get('/videos', (_req, res) => {
-    res.json(videoList);
+    const videoList = data.slice().shift();
+    res.send(videoList);
 })
 
 // GET /videos/:id
 app.get('/videos/:id', (req, res) => {
     const videoId = req.params.id;
-    res.json(videoDetail.filter(video => {
+    const videoDetails = data.slice().pop();
+    res.send(videoDetails.filter(video => {
         return video.id === videoId;
     }).pop())
+})
+
+// POST /videos
+app.post('/videos', (req, res) => {
+    const videoList = data.slice().shift();
+    const videoDetails = data.slice().pop();
+    videoList.push({
+        id: req.body.id,
+        title: req.body.title,
+        channel: req.body.channel,
+        image: req.body.image});
+    videoDetails.push(req.body);
+    return res.status(201).send(videoList);
 })
 
 
